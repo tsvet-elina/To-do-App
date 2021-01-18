124f8b33c03645ed6368f9e385f967a041b281db
tsvet-elina <panayotova.tsvetelina@gmail.com>
2021-01-18 16:39:47 +0200

    Added DatabaseHelper and NoteHandler.

---
+++ app/src/main/java/com/example/todoapp/DatabaseHelper.java
@@ -0,0 +1,31 @@
+package com.example.todoapp;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    String sqlQuery;
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "'NotesDatabase";
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        sqlQuery = "CREATE TABLE Note (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, description TEXT)";
+        db.execSQL(sqlQuery);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        sqlQuery = "DROP TABLE IF EXISTS Note";
+        db.execSQL(sqlQuery);
+        onCreate(db);
+    }
+}
